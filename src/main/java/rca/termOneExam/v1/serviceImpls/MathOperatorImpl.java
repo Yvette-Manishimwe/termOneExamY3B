cc0d667351d67035a4723c3e5df6aff21c5c2564
Yvette-Manishimwe <yvettemanishimwe9@gmail.com>
2023-12-18 09:33:38 +0200

    adding a post method

--- src/main/java/rca/termOneExam/v1/serviceImpls/MathOperatorImpl.java
+++ src/main/java/rca/termOneExam/v1/serviceImpls/MathOperatorImpl.java
@@ -14,22 +14,19 @@ public class MathOperatorImpl implements IMathOperator {
         }
 
         switch (operation) {
-            case "*":
-                return operand1 * operand2;
-            case "/":
-                return operand1 / operand2;
-            case "+":
-                return operand1 + operand2;
-            case "-":
-                return operand1 - operand2;
-            case "**":
-                return Math.pow(operand1, operand2);
-            case "log":
-                return operand1 * Math.log10(operand2);
-            case "ln":
-                return operand1 * Math.log(operand2);
-            default:
-                throw new RuntimeException("Unknown Operation");
+
+                case "*":
+                    return operand1 * operand2;
+                case "/":
+                    return operand1 / operand2;
+                case "+":
+                    return operand1 + operand2;
+                case "-":
+                    return operand1 - operand2;
+                default:
+                    throw new RuntimeException("Unknown operation");
+            }
         }
     }
-}
+
+
