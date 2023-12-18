c8eb9e0819facfd0cad4fd4103a35950c3364df5
Yvette-Manishimwe <yvettemanishimwe9@gmail.com>
2023-12-18 09:42:19 +0200

    adding unit testing

--- src/test/java/rca/termOneExam/service/MathOperatorServiceTest.java
+++ src/test/java/rca/termOneExam/service/MathOperatorServiceTest.java
@@ -8,7 +8,6 @@ import rca.termOneExam.v1.exceptions.InvalidOperationException;
 import rca.termOneExam.v1.serviceImpls.MathOperatorImpl;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,17 +17,58 @@ public class MathOperatorServiceTest {
     private MathOperatorImpl mathOperatorService;
 
     @Test
-    public void should_create_math_operation_success() throws InvalidOperationException {
+    public void should_create_math_operation_addition_success() throws InvalidOperationException {
         double operator1 = 4;
         double operator2 = 9;
         String operation = "+";
 
-        double mathOperation = mathOperatorService.doMath(operator1,operator2, operation);
+        double mathOperation = mathOperatorService.doMath(operator1, operator2, operation);
         when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(mathOperation);
 
         double actualMathOperator = mathOperatorService.doMath(operator1, operator2, operation);
 
-        assertThat(mathOperation).usingRecursiveComparison().isEqualTo(actualMathOperator);
+        assertThat(mathOperation).isEqualTo(actualMathOperator);
     }
 
+    @Test
+    public void should_create_math_operation_subtraction_success() throws InvalidOperationException {
+        double operator1 = 10;
+        double operator2 = 5;
+        String operation = "-";
+
+        double mathOperation = mathOperatorService.doMath(operator1, operator2, operation);
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(mathOperation);
+
+        double actualMathOperator = mathOperatorService.doMath(operator1, operator2, operation);
+
+        assertThat(mathOperation).isEqualTo(actualMathOperator);
+    }
+
+    @Test
+    public void should_create_math_operation_multiplication_success() throws InvalidOperationException {
+        double operator1 = 6;
+        double operator2 = 7;
+        String operation = "*";
+
+        double mathOperation = mathOperatorService.doMath(operator1, operator2, operation);
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(mathOperation);
+
+        double actualMathOperator = mathOperatorService.doMath(operator1, operator2, operation);
+
+        assertThat(mathOperation).isEqualTo(actualMathOperator);
+    }
+
+    @Test
+    public void should_create_math_operation_division_success() throws InvalidOperationException {
+        double operator1 = 20;
+        double operator2 = 4;
+        String operation = "/";
+
+        double mathOperation = mathOperatorService.doMath(operator1, operator2, operation);
+        when(mathOperatorService.doMath(operator1, operator2, operation)).thenReturn(mathOperation);
+
+        double actualMathOperator = mathOperatorService.doMath(operator1, operator2, operation);
+
+        assertThat(mathOperation).isEqualTo(actualMathOperator);
+    }
 }
